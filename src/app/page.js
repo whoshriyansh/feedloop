4d4cf7be2061ba4079b3c0e3b7770c2bd9db35ab
Shriyansh Lohia <shriyanshlohia0@gmail.com>
2025-08-22 07:24:26 +0530

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-primary h-screen w-screen">
+      <h1>Hello FeedLoop</h1>
+    </div>
+  );
+}
